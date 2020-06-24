5150852587cccf24655da8778162c612e269534d
Danica Fisher <dlwylin@gmail.com>
2020-06-24 06:48:27 -0700

    Adds puppetter to circleCI

--- test.js
+++ test.js
@@ -1,3 +1,35 @@
+// const app = require('./server/index.js');
+// const mongoose = require('mongoose');
+// const Site = require('./database/Site.js');
+// const db = require('./database/index.js');
+
+
+
+// describe('test passing GET request', () => {
+  // afterAll(async () => {
+  //   await db.close();
+  // });
+
+  // it('Checks that GET returns correct site based on id', async () => {
+  //   const res = app.get('/site/82');
+  //   const resBodyObj = JSON.parse(res.body);
+  //   expect(resBodyObj.id).toEqual(82);
+
+  // });
+
+
+
+//   test('Checks that GET returns Not Found based on false id', async () => {
+//     const res = await page.goto('http://localhost:3002/site/182');
+//     const resBody = await res.text();
+//     expect(resBody).toEqual('Not found');
+
+//   });
+
+
+// });
+
+
 const puppeteer = require('puppeteer');
 const pageURL = 'http://localhost:3002/';
 
@@ -10,15 +42,15 @@ beforeAll(async () => {
   browser = await puppeteer.launch({
     headless: false,
     slowMo: 100,
-    args: [`--window-size=${width}, ${height}`]
+    args: [`--window-size=${width}, ${height}, -–no-sandbox, --disable-setuid-sandbox`]
   });
   page = await browser.newPage();
   await page.setViewport({width, height});
 });
 
-// afterAll(() => {
-//   browser.close();
-// });
+afterAll(() => {
+  browser.close();
+});
 
 describe('test passing GET request', () => {
 
