636be5ca8d6f99ca601be216ab230d701b2cae5b
Danica Fisher <dlwylin@gmail.com>
2020-06-27 12:28:27 -0700

    Removes popup library

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
-import Popup from 'reactjs-popup';
 import CampCard from './CampCard.jsx';
 import EssCard from './EssCard.jsx';
 import AmntCard from './AmntCard.jsx';
